45b62d0b8ffcbc31bb549e78f3fecae3c4d99c31
Jonathan Ng <72720297+JonathanCNg@users.noreply.github.com>
2022-11-09 14:36:26 -0800

    Update M1.py
    Co-Authored-By: crbonham <80612098+crbonham@users.noreply.github.com>

--- M1.py
+++ M1.py
@@ -1,21 +1,19 @@
 import os
 import json
 import nltk
-from nltk.corpus import stopwords, words
-nltk.download('stopwords')
-nltk.download('words')
-from unicodedata import category
-import sys
-import pickle
+from bs4 import BeautifulSoup
 
 directs = ["www-db_ics_uci_edu", "www_informatics_uci_edu", "www_cs_uci_edu"]
-stop_words = set(stopwords.words('english'))
-eng_words = set(words.words())
-codepoints = range(sys.maxunicode + 1)
-punctuation = {c for i in codepoints if category(c := chr(i)).startswith("P")}  #implemented with guidance from: https://stackoverflow.com/questions/60983836/complete-set-of-punctuation-marks-for-python-not-just-ascii
+directs = ["www_informatics_uci_edu"]
 
+for i in range(len(directs)):
+    directs[i] = "DEV/" + directs[i]
+
+print(directs)
 
 index = {}
+
+ps = nltk.stem.PorterStemmer()
 
 for d in directs:
     directory = os.fsencode(d)
@@ -24,20 +22,27 @@ for d in directs:
         f = open(d+"/"+fname, "r")
         data = json.load(f)
         f.close()
-        tokens = nltk.word_tokenize(data['content'])
-        tokens_cleaned = [word.lower() for word in tokens if word.lower() not in stop_words and word.lower() not in punctuation and word.lower() in eng_words]
-        for token in tokens_cleaned:
-            if token in index.keys():
-                if fname in index[token].keys():
-                    index[token][fname] += 1
-                else:
-                    index[token][fname] = 1
-            else:
-                index[token] = {fname:1}
-        print(fname, " is Indexed")
-    print(d, " is done")
+        
+        soup = BeautifulSoup(data['content'], 'html.parser')
+        tokens = nltk.word_tokenize(soup.get_text(' ', strip=True))
+        print(data['content'])
+        print(data['url'])
+        print(tokens)
+        exit(0)
+#         tokens = nltk.word_tokenize(data['content'])
+#         for token in tokens:
+#             token = ps.stem(token)
+#             if token in index.keys():
+#                 if fname in index[token].keys():
+#                     index[token][fname] += 1
+#                 else:
+#                     index[token][fname] = 1
+#             else:
+#                 index[token] = {fname:1}
+#         print(fname, " is Indexed")
+#     print(d, " is done")
 # file = open("index", "wb")
 # pickle.dump(index, file)
 # file.close()
-print("Unique ")
+# print(index)
 
