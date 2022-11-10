78eb60c474852df91e050732abe97aa45390a2ae
hackerrank <jonng1234@gmail.com>
2022-11-09 21:46:53 -0800

    Update M1.py

--- M1.py
+++ M1.py
@@ -6,18 +6,19 @@ import math
 from bs4 import BeautifulSoup
 from scraper import is_valid
 
-# directs = os.listdir("DEV")
+directs = os.listdir("DEV")
 # directs = ["www-db_ics_uci_edu", "www_informatics_uci_edu", "www_cs_uci_edu"]
-directs = ["www-db_ics_uci_edu"]
+# directs = ["www-db_ics_uci_edu"]
 
 for i in range(len(directs)):
     directs[i] = "DEV/" + directs[i]
 
 index = {}
-page_count = {} # Key is a token, value is a set of urls
 total_pages = 0
 word_count = {}
 ps = nltk.stem.PorterStemmer()
+
+print("\n" + "{:<50}".format("URL") + "PROGRESS")
 
 for d in directs:
     directory = os.fsencode(d)
@@ -31,18 +32,16 @@ for d in directs:
         f.close()
         url = data['url']
         try:
+            # TODO: Figure out whether we're supposed to use is_valid and whether our is_valid is too aggressive
             if is_valid(url):
+                # TODO: What happens when we use BeautifulSoup on non-HTML? 🤔
                 soup = BeautifulSoup(data['content'], 'html.parser').get_text(' ', strip=True)
+                # TODO: Make tokenizer than works only for alphanumeric (NLTK includes symbols and contractions)
                 tokens = nltk.word_tokenize(soup)
                 word_count[url] = len(tokens)
 
                 for token in tokens:
                     token = ps.stem(token)
-
-                    if token not in page_count:
-                        page_count[token] = {url}
-                    else:
-                        page_count[token].add(url)
 
                     if token in index.keys():
                         if url in index[token].keys():
@@ -53,7 +52,7 @@ for d in directs:
                         index[token] = {url:1}
             counter += 1
             # print("\r" + str(counter) + " " + fname + " AKA " + url + " is indexed".ljust(100)[:100], end="")
-            print("\r" + "{:<50}".format(d) + " | ", end="")
+            print("\r" + "{:<50}".format(d) + "| ", end="")
             out_of_10 = counter*10//len(files)
             for i in range(out_of_10):
                 print("# ", end="")
@@ -76,7 +75,7 @@ file.close()
 # with open("delete_me.json", "w") as f:
 #     json.dump(index, f)
 with open("output.txt", "w") as f:
-    f.write("# of Indexed Documents: " + str(len(page_count)) + "\n")
+    f.write("# of Indexed Documents: " + str(total_pages) + "\n")
     f.write("# of Unique Tokens: " + str(len(index)) + "\n")
     f.write("Size of Index on Disk: " + str(os.path.getsize("index")//1000) + " KB")
 
@@ -84,9 +83,9 @@ with open("output.txt", "r") as f:
     print("\n ~ STATS ~ ")
     for line in f:
         print(line, end="")
+    print()
 
 # print(index)
 
-# TODO: Make tokenizer than works only for alphanumeric (NLTK includes symbols and contractions)
-# TODO: Figure out whether we're supposed to use is_valid and whether our is_valid is too aggressive
-# TODO: What happens when we use BeautifulSoup on non-HTML? 🤔
+
+
