6319a4ca36d6e6cca8458ad57edee477a697f33d
Jonathan Ng <72720297+JonathanCNg@users.noreply.github.com>
2022-11-09 16:30:09 -0800

    Update M1.py

--- M1.py
+++ M1.py
@@ -3,14 +3,13 @@ import json
 import nltk
 import pickle
 from bs4 import BeautifulSoup
+from scraper import is_valid
 
 directs = ["www-db_ics_uci_edu", "www_informatics_uci_edu", "www_cs_uci_edu"]
 directs = ["www_cs_uci_edu"]
 
 for i in range(len(directs)):
     directs[i] = "DEV/" + directs[i]
-
-print(directs)
 
 index = {}
 
@@ -23,18 +22,21 @@ for d in directs:
         f = open(d+"/"+fname, "r")
         data = json.load(f)
         f.close()
+        url = data['url']
         soup = BeautifulSoup(data['content'], 'html.parser').get_text(' ', strip=True)
         tokens = nltk.word_tokenize(soup)
         for token in tokens:
             token = ps.stem(token)
             if token in index.keys():
-                if fname in index[token].keys():
-                    index[token][fname] += 1
+                if url in index[token].keys():
+                    index[token][url] += 1
                 else:
-                    index[token][fname] = 1
+                    index[token][url] = 1
             else:
-                index[token] = {fname:1}
-        print(fname, " is Indexed")
+                index[token] = {url:1}
+                print(index)
+                exit(0)
+        print(fname, "AKA", url, " is Indexed")
     print(d, " is done")
 file = open("index", "wb")
 pickle.dump(index, file)
