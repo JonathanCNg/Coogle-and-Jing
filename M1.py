b5879291950a806c162671f9ddadd89333dac7af
hackerrank <jonng1234@gmail.com>
2022-11-09 20:43:08 -0800

    Update M1.py

--- M1.py
+++ M1.py
@@ -26,34 +26,37 @@ for d in directs:
         data = json.load(f)
         f.close()
         url = data['url']
-        if is_valid(url):
-            soup = BeautifulSoup(data['content'], 'html.parser').get_text(' ', strip=True)
-            tokens = nltk.word_tokenize(soup)
+        try:
+            if is_valid(url):
+                soup = BeautifulSoup(data['content'], 'html.parser').get_text(' ', strip=True)
+                tokens = nltk.word_tokenize(soup)
 
-            for token in tokens:
-                token = ps.stem(token)
+                for token in tokens:
+                    token = ps.stem(token)
 
-                if token not in page_count:
-                    page_count[token] = {url}
-                else:
-                    page_count[token].add(url)
-
-                if token in index.keys():
-                    if url in index[token].keys():
-                        index[token][url] += 1
+                    if token not in page_count:
+                        page_count[token] = {url}
                     else:
-                        index[token][url] = 1
-                else:
-                    index[token] = {url:1}
-        counter += 1
-        # print("\r" + str(counter) + " " + fname + " AKA " + url + " is indexed".ljust(100)[:100], end="")
-        print("\r" + "{:<50}".format(d) + " | ", end="")
-        out_of_10 = counter*10//len(files)
-        for i in range(out_of_10):
-            print("# ", end="")
-        for i in range(10-out_of_10):
-            print("  ", end="")
-        print("| " + str(counter) + "/" + str(len(files)), end="")
+                        page_count[token].add(url)
+
+                    if token in index.keys():
+                        if url in index[token].keys():
+                            index[token][url] += 1
+                        else:
+                            index[token][url] = 1
+                    else:
+                        index[token] = {url:1}
+            counter += 1
+            # print("\r" + str(counter) + " " + fname + " AKA " + url + " is indexed".ljust(100)[:100], end="")
+            print("\r" + "{:<50}".format(d) + " | ", end="")
+            out_of_10 = counter*10//len(files)
+            for i in range(out_of_10):
+                print("# ", end="")
+            for i in range(10-out_of_10):
+                print("  ", end="")
+            print("| " + str(counter) + "/" + str(len(files)), end="")
+        except Exception as e:
+            print("EXCEPTION THROWN: Tried", url, "got", e)
     print()
 
 for token in index:
